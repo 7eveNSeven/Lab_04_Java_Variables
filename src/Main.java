e2c496e6fc3817f408fe6c647bdbdd6976cf830b
tylersmith <smit2t7@mail.uc.edu>
2024-09-23 22:14:17 -0400

    Completed Part 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
-public class Main {
+public class Main
+{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int intOperandA = 41;
+        int intOperandB = 23;
+
+        int intSum = 0;
+        int intProduct = 0;
+        int intDifference = 0;
+        int intQuotient = 0;
+        int intRemainder = 0;
+
+        double doubleOperandA = 12.41;
+        double doubleOperandB = 5.23;
+
+        double doubleSum = 0;
+        double doubleProduct = 0;
+        double doubleDifference = 0;
+        double doubleQuotient = 0;
+        double doubleRemainder = 0;
+
+        intSum = intOperandA + intOperandB;
+        System.out.printf("The sum using two ints of " + intOperandA + " and " + intOperandB + " is " + intSum);
+
+        intProduct = intOperandA * intOperandB;
+        System.out.printf("The product using two ints of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+
+
+   intDifference = intOperandA - intOperandB;
+   intQuotient = intOperandA / intOperandB;
+   intRemainder = intOperandA % intOperandB;
+
+   double lunchCost= 9.70;
+
+
     }
 }
